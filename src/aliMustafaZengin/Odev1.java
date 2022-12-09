0de503691e2617b91fa6e36f828b851427a7ad71
ChargeAhead <omertufan26@yandex.com>
2022-12-09 00:07:27 +0000

    Birinci odevim.

---
+++ src/aliMustafaZengin/Odev1.java
@@ -0,0 +1,16 @@
+package aliMustafaZengin;
+
+public class Odev1 {
+
+	public static void main(String[] args) {
+		
+		
+		//Odevim
+		
+		int a = 1;
+		
+		System.out.println("a degeri: "+a);
+
+	}
+
+}
