a412fda7bab393ac1879f76510548e205124ac96
Lenovo <Lenovo@DESKTOP-A4H2E5S.home>
2022-12-15 11:52:14 +0200

    3. Hafta Odevi

---
+++ src/yasarYilmaz/Odev3HaftaCevaplar.java
@@ -0,0 +1,122 @@
+package yasarYilmaz;
+
+import java.util.Scanner;
+
+public class Odev3HaftaCevaplar {
+
+	public static void main(String[] args) {
+
+/*		
+		 Scanner verigir = new Scanner(System.in);
+
+	        System.out.println("Girilen 3 Sayıdan En Büyüğünü Bulan program. \n 3 Adet Sayi Giriniz ");
+	        int num1 = verigir.nextInt();
+	        int num2 = verigir.nextInt();
+	        int num3 = verigir.nextInt();
+	     if ((num1>num2) && (num1>num3)) {
+	    	 System.out.println("En Büyük Sayı = " + num1);
+	     }
+	     else if ((num2>num1) && (num2>num3)) {
+	    	 System.out.println("En Büyük Sayı = " + num2);
+	     }
+	     else System.out.println("En Büyük Sayı = " + num3);
+		
+ */
+
+		 // ************************************************************************************		 
+		
+/*
+		 Scanner getday = new Scanner(System.in);
+		
+			System.out.println("Haftanın  Bir Gününü Giriniz.");
+			String day = getday.nextLine();
+System.out.println(day.equals("cumartesi")||day.equals("pazar") ? 
+( day.equals("cumartesi") ? "cumartesi ve haftasonu":(day.equals("pazar") ? "pazar ve haftasonu": "haftaiçi")) : "haftaiçi" );
+	   
+*/
+		 //************************************************************************************		 
+
+/*
+		 Scanner getday = new Scanner(System.in);
+		 System.out.println("Haftanın Günlerinden İlk Harfi Girilenleri Yazdırma ");
+		 String day = getday.nextLine();
+		 	switch(day) 
+		 	 {
+		 		case "p":
+		 			System.out.println("Pazartesi\n Pazar \n Pazar");
+		 			break;
+		 		case "s":
+		 			System.out.println("Salı");
+		 			break;
+		 		case "c":
+		 			System.out.println("Çarşamba \n  Cuma  \n  Cumartesi");
+		 			break;
+		 		default:
+		 			System.out.println("Yanlış Harf Girdiniz");
+		 	 }
+*/
+		//************************************************************************************		 
+/*
+		 Scanner getday = new Scanner(System.in);
+		 System.out.println("3 Kutsal Kitaba Göre Kutsal Günleri Giriniz ");
+		 String day = getday.nextLine();
+		 	switch(day) 
+		 	 {
+		 		case "cuma":
+		 			System.out.println(day +" Müslümanların Mübarek Günü");
+		 			break;
+		 		case "cumartesi":
+		 			System.out.println(day + " Hristiyanların Mübarek Günü");
+		 			break;
+		 		case "pazar":
+		 			System.out.println(day + " Yahudilerin Mübarek Günü");
+		 			break;
+		 		default:
+		 			System.out.println(" Yanlış Gün Girdiniz");
+		 	 }
+*/
+		//************************************************************************************
+		 
+/*		 
+		    Scanner keys= new Scanner(System.in);
+			 	System.out.println("bir karakter giriniz");
+		        String str= keys.nextLine();
+		        char ch=str.charAt(0);
+			        if ( (ch>=65 && ch<=90)|| (ch>=97 && ch<=122) ) 	           
+			            System.out.println(ch+ " bir harftir.");
+			        else 
+			        	System.out.println(ch+" bir harf değildir");
+			        int i3= ch;
+			        System.out.println(i3);
+
+ 
+*/
+
+/*		
+		//************************************************************************************		
+		
+		    Scanner notlar = new Scanner(System.in);
+				System.out.println("Notunuzu Giriniz ");
+			    int puan = notlar.nextInt();
+				    if (puan>80) System.out.println("Notunuz  A");
+		            else if (puan>60) System.out.println("Notunuz  B");
+		            else if (puan>50) System.out.println("Notunuz  C");
+		            else System.out.println("Notunuz  D");
+			
+*/	
+		//************************************************************************************
+				    
+			Scanner sayi = new Scanner(System.in);
+			 	System.out.println("lütfen 4 basamaklı bir sayı giriniz!!");
+	        		int rakam= sayi.nextInt();
+	        		String str= rakam % 5 == 0  ? (rakam % 2==0 ? "5'e bölünen çift sayı" : "5'e bölünen tek sayı" ): "tekrar deneyin";
+	        		System.out.println(str);
+
+	}
+		 
+}
+
+	
+	
+
+
--- src/yasarYilmaz/odev2hafta.java
+++ src/yasarYilmaz/odev2hafta.java
@@ -8,33 +8,33 @@ public class odev2hafta {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		// Adınızı Soyadınızı ve Adresinizi Girip Ekrana Yazdırın
-		System.out.println("İsminizi Giriniz ");
-		String ad = scan.next();
-		System.out.println("Soyadınızı Giriniz ");
-		String soyad = scan.next();
-		System.out.println("İsminiz          = " + ad);
-		System.out.println("Soyisminiz       = " + soyad);
-		System.out.println("Kursumuza katiliminiz alinmistir, tesekkur ederiz");
-		
-		// Girilen Saati Saniye Olarak Hesaplama
-		System.out.println("Saati Girerek Saniye Hesaplama ");
-		System.out.println("Saat Giriniz  ");
-		Long saat = scan.nextLong();
-		System.out.println(saat + " Saat = " + saat*60*60 + " Saniyedir" );
-	
-		// Karenin Alan ve Çevresinin Hesaplanması
-		System.out.println("Karenin Alan ve Çevresini Hesaplama ");
-		System.out.println("Kenar Uzunluğunu Giriniz  ");
-		Short kenar = scan.nextShort();
-		System.out.println(kenar + " Alan = " + kenar*kenar + "\n Çevre = " + kenar*4 );
-		
-		// Girilen Mil'i Km ye Çevirme
-		System.out.println("Uzunluğu mil girerek km hesaplama ");
-		System.out.println("Uzunluğu mil giriniz  ");
-		Double mil = scan.nextDouble();
-		System.out.println(mil + " mil = " + mil*1.6);
-		
+			// Adınızı Soyadınızı ve Adresinizi Girip Ekrana Yazdırın
+			System.out.print("İsminizi Giriniz ");
+				String ad = scan.next();
+			System.out.print("Soyadınızı Giriniz \r");
+				String soyad = scan.next();
+			System.out.println("İsminiz          = " + ad);
+			System.out.println("Soyisminiz       = " + soyad);
+			System.out.println("Kursumuza katiliminiz alinmistir, tesekkur ederiz");
+
+			// Girilen Saati Saniye Olarak Hesaplama
+			System.out.println("Saati Girerek Saniye Hesaplama ");
+			System.out.print("Saat Giriniz  ");
+				Long saat = scan.nextLong();
+			System.out.println(saat + " Saat = " + saat*60*60 + " Saniyedir" );
+
+			// Karenin Alan ve Çevresinin Hesaplanması
+			System.out.println("Karenin Alan ve Çevresini Hesaplama ");
+			System.out.print("Kenar Uzunluğunu Giriniz  ");
+				Short kenar = scan.nextShort();
+			System.out.println(kenar + " Alan = " + kenar*kenar + "\n Çevre = " + kenar*4 );
+			
+			// Girilen Mil'i Km ye Çevirme
+			System.out.println("Uzunluğu mil girerek km hesaplama ");
+			System.out.print("Uzunluğu mil giriniz  ");
+				Double mil = scan.nextDouble();
+			System.out.println(mil + " mil = " + mil*1.6);
+			
 		
 		
 }
