b3541830c8c95381276872e4cf206f8a92b7e465
Lenovo <Lenovo@DESKTOP-A4H2E5S.home>
2022-12-19 21:57:50 +0200

    4 Hafta Odev

--- src/yasarYilmaz/Odev4Hafta.java
+++ src/yasarYilmaz/Odev4Hafta.java
@@ -23,16 +23,15 @@ public class Odev4Hafta {
 
 		/*
 		 * Scanner scan = new Scanner(System.in);
-		 * System.out.println("Adınızı Giriniz  "); 
-		 * String ad = scan.nextLine();
-		 * boolean a_varmi=ad.contains("a"); 
-		 * boolean Z_varmi=ad.contains("Z"); 
-		 * if (!(a_varmi || Z_varmi)) System.out.println("Adınız  a veya Z Harfi İçermiyor"); 
-		 * if (a_varmi) System.out.println("Adınızın içinde  a  harfi vardır"); 
-		 * if (Z_varmi) System.out.println("Adınızın içinde  Z  harfi vardır");
-		 */		
+		 * System.out.println("Adınızı Giriniz  "); String ad = scan.nextLine(); boolean
+		 * a_varmi=ad.contains("a"); boolean Z_varmi=ad.contains("Z"); if (!(a_varmi ||
+		 * Z_varmi)) System.out.println("Adınız  a veya Z Harfi İçermiyor"); else
+		 * System.out.println("Adınız  a veya Z Harfi İçeriyor"); if (a_varmi)
+		 * System.out.println("Adınızın içinde  a  harfi vardır"); if (Z_varmi)
+		 * System.out.println("Adınızın içinde  Z  harfi vardır");
+		 */	 		
 		
-//Soru 3) Kullanicidan isim ve soyismini isteyin ve hangisinin daha uzun oldugunu azdirin.
+//Soru 3) Kullanicidan isim ve soyismini isteyin ve hangisinin daha uzun oldugunu yazdirin.
 		
 		/*
 		 * Scanner scan = new Scanner(System.in);
@@ -41,7 +40,8 @@ public class Odev4Hafta {
 		 * if (ad.length()>soyad.length())
 		 * System.out.println("Adınız  Soyadınız'dan Daha Uzun"); else
 		 * System.out.println("Soyadınız  Adınız'dan daha Uzun");
-		 */		
+		 */
+		 		
 		
 		
 //Soru 4) Kullanicidan ismini, soyismini ve kredi karti bilgisini isteyin ve asagidaki gibi yazdirin.
@@ -51,6 +51,7 @@ public class Odev4Hafta {
 		//substring ==> girdigimiz indexler arasindaki ifadeleri gormek icin kullaniriz.
 			
 		 
+		
 		/*
 		 * Scanner scan = new Scanner(System.in);
 		 * System.out.println("Adınızı Giriniz  "); String ad = scan.nextLine();
@@ -58,9 +59,9 @@ public class Odev4Hafta {
 		 * System.out.println("Kredi Kartı Numarasını Giriniz  "); String kredikartno =
 		 * scan.nextLine(); System.out.println("Ad Soyad = " + ad.substring(0,1) +
 		 * "***  " + soyad.substring(0, 1) + "*****\n" +
-		 * "Kredi Kartııwu Numarası = **** **** **** " +
+		 * "Kredi Kartı Numarası = **** **** **** " +
 		 * kredikartno.substring((kredikartno.length()-4),kredikartno.length()));
-		 */		 
+		 */		 		 		 
 
 //Aşağıdaki sorunun zorluk seviyesi yüksektir. Kısmi çözümlerde kabul edilir. 
 //Soru 5) Kullanicidan bir sifre girmesini isteyin. Asagidaki sartlari sagliyorsa “Sifre 
@@ -73,35 +74,38 @@ public class Odev4Hafta {
 		
 		
 		
-//		  Scanner scan = new Scanner(System.in);
-//		  System.out.println("Şifrenizi Giriniz  "); String sifre = scan.nextLine();
-//		  int uzunluk = sifre.length(); char ilkharf = sifre.charAt(0); char sonharf =
-//		  sifre.charAt(sifre.length()-1); char ctrlilk = sifre.toUpperCase().charAt(0);
-//		  char ctrlson = sifre.toLowerCase().charAt(sifre.length()-1); 
-//		  // boolean ctrlbosluk = sifre.isBlank(); String ctrlsifre = sifre.replace(" ", "a");
-//		  boolean ctrlbosluk = sifre.contains(" ");
-//		  
-//		  
-//		  if (uzunluk >= 8) System.out.println("Şifre 8 Karekterli veya Daha Uzun\n ");
-//		  System.out.println(ilkharf == ctrlilk ? "Şifrenin İlk Harfi Büyüktür-> " +
-//		  ilkharf: "Şifrenin İlk Harfi Küçüktür-> " + ilkharf );
-//		  System.out.println(sonharf == ctrlson ? "Şifrenin Son Harfi Küçüktür-> " +
-//		  sonharf : "Şifrenin Son Harfi Büyüktür-> " + sonharf );
-//		  
-//		  if ( (uzunluk >= 8) && (ilkharf == ctrlilk) && (sonharf == ctrlson) && (sifre
-//		  == ctrlsifre)) System.out.println("\nŞifre Başarı İle Tamamlandı"); else
+		  Scanner scan = new Scanner(System.in);
+		  System.out.println("Şifrenizi Giriniz  "); 
+		  String sifre = scan.nextLine();
+		  int uzunluk = sifre.length(); 
+		  char ilkharf = sifre.charAt(0); 
+		  char sonharf = sifre.charAt(sifre.length()-1); 
+		  char ctrlilk = sifre.toUpperCase().charAt(0);
+		  char ctrlson = sifre.toLowerCase().charAt(sifre.length()-1); 
+		  // boolean ctrlbosluk = sifre.isBlank(); String ctrlsifre = sifre.replace(" ", "a");
+		  boolean ctrlbosluk = sifre.contains(" ");
+		  
+		  
+		  if (uzunluk >= 8) System.out.println("Şifre 8 Karekterli veya Daha Uzun\n ");
+		  System.out.println(ilkharf == ctrlilk ? "Şifrenin İlk Harfi Büyüktür-> " +
+		  ilkharf: "Şifrenin İlk Harfi Küçüktür-> " + ilkharf );
+		  System.out.println(sonharf == ctrlson ? "Şifrenin Son Harfi Küçüktür-> " +
+		  sonharf : "Şifrenin Son Harfi Büyüktür-> " + sonharf );
+		  
+//		  if ( (uzunluk >= 8) && (ilkharf == ctrlilk) && (sonharf == ctrlson) && (sifre == ctrlsifre)) 
+//		  System.out.println("\nŞifre Başarı İle Tamamlandı"); else
 //		  System.out.println("\nsdfİşlem Başarısız. Tekrar Şifre Giriniz");
-		 
-//		  if ( (uzunluk >= 8) && (ilkharf == ctrlilk) && (sonharf == ctrlson) && (!ctrlbosluk)) 
-//			  System.out.println("\nŞifre Başarı İle Tamamlandı"); else
-//				  System.out.println("\nİşlem Başarısız. Tekrar Şifre Giriniz");
-		
+//		 
+		  if ( (uzunluk >= 8) && (ilkharf == ctrlilk) && (sonharf == ctrlson) && (!ctrlbosluk)) 
+			  System.out.println("\nŞifre Başarı İle Tamamlandı"); else
+				  System.out.println("\nİşlem Başarısız. Tekrar Şifre Giriniz");
+//		
 //		System.out.println("ilk Harf =" + ilkharf + "\nSon Harf = " + sonharf + "\nŞifre Uzunluğu = " + uzunluk);
 //		System.out.println("Sifre = " + sifre + "\nctrlsifre = " + ctrlsifre);		
 		
 		
-//		if (ilkharf == ctrlilk) System.out.println("Şifrenin İlk Harfi Büyüktür " + ilkharf);
-//		if (sonharf == ctrlson) System.out.println("Şifrenin Son Harfi Küçüktür "  + sonharf);
+		if (ilkharf == ctrlilk) System.out.println("Şifrenin İlk Harfi Büyüktür " + ilkharf);
+		if (sonharf == ctrlson) System.out.println("Şifrenin Son Harfi Küçüktür "  + sonharf);
 
 
 		
