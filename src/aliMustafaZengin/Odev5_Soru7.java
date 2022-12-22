f187e728f42be45c26ef729bbec74fc6ed1b4bcd
Jasi <Jasi@Asi1987>
2022-12-22 21:44:26 +0100

    odev5c

--- src/aliMustafaZengin/Odev5_Soru7.java
+++ src/aliMustafaZengin/Odev5_Soru7.java
@@ -11,26 +11,27 @@ public class Odev5_Soru7 {
 		//girdigi pozitif sayilarin toplaminin kac oldugunu yazdirin.(do while dongusu ile)
 		
 		
-//		Scanner scan=new Scanner(System.in);
-//		System.out.println("Pozitif sayi girin");
-//		
-//		
-//		int a=0;
-//		int sayac=0;
-//		
-//		do {
-//			int sayi=scan.nextInt();
-//			
-//			
-//			if(sayi>0) {
-//				a=a+sayi;
-//				sayac++;
-//			}
-//			
-//		} 
-//		while (sayi!=0);
-//		System.out.println(a);
-//		System.out.println(sayac);
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Pozitif sayi girin");
+		
+		
+		int a=0;
+		int sayac=0;
+		int sayi=0;
+		
+		do {
+			sayi=scan.nextInt();
+			
+			
+			if(sayi>0) {
+				a=a+sayi;
+				sayac++;
+			}
+			
+		} 
+		while (sayi!=0);
+		System.out.println(a);
+		System.out.println(sayac);
 
 
 	}
