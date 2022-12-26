d7f29786103668350b9f6b818d266b65c766be95
ramazan <ramaz@DESKTOP-5G57CJS.fritz.box>
2022-12-26 21:46:42 +0100

    6.Hafta ödevim güncelleme.

--- src/ramazanKombe/Odev6Telefon.java
+++ src/ramazanKombe/Odev6Telefon.java
@@ -5,32 +5,39 @@ import java.util.Scanner;
 public class Odev6Telefon {
 
 	public static void main(String[] args) {
+		
 
+		/*1.	E-ticaret sitelerini inceleyiniz.
+		 *  Bir urunun(objenin) ne gibi ozellikleri olduguna bakiniz.
+		 *   Bir tane class olusturarak bu ozellikleri kullanan 15 tane urun hazirlayiniz.
+		 *    (5 tanesi kullanicidan veri alinarak , 5 tanesi parametreli constructor kullanilarak,
+		 *     5 tanesi bos constructor kullanilarak olmali)
+		 */
 
 
 	Odev6Obje urun1 = new Odev6Obje("Samsung", "Galaxy", "Rosa", 512, 600, 78);
 
-	System.out.println(urun1.marka + " " + urun1.model + " " + urun1.renk + " " + urun1.hafiza + " " + urun1.fiyat
-			+ " " + urun1.stok);
+	System.out.println(urun1.marka + " " + urun1.model + " " + urun1.renk + " "
+						+ urun1.hafiza + " " + urun1.fiyat + " " + urun1.stok);
 
 	Odev6Obje urun2 = new Odev6Obje("Samsung", "Galaxy", "Gold", 128, 500, 100);
 
-	System.out.println(urun2.marka + " " + urun2.model + " " + urun2.renk + " " + urun2.hafiza + " " + urun2.fiyat
-			+ " " + urun2.stok);
+	System.out.println(urun2.marka + " " + urun2.model + " " + urun2.renk + " " 
+						+ urun2.hafiza + " " + urun2.fiyat + " " + urun2.stok);
 
-		Odev6Obje urun3 = new Odev6Obje("Lenovo", "Legion", "Siyah", 256, 400, 200);
+	Odev6Obje urun3 = new Odev6Obje("Lenovo", "Legion", "Siyah", 256, 400, 200);
 
-	System.out.println(urun3.marka + " " + urun3.model + " " + urun3.renk + " " + urun3.hafiza + " " + urun3.fiyat
-			+ " " + urun3.stok);
+	System.out.println(urun3.marka + " " + urun3.model + " " + urun3.renk + " " 
+						+ urun3.hafiza + " " + urun3.fiyat + " " + urun3.stok);
 
 	Odev6Obje urun4 = new Odev6Obje("Iphone", "13 Promax", "Rosa", 512, 900, 80);
 
-	System.out.println(urun4.marka + " " + urun4.model + " " + urun4.renk + " " + urun4.hafiza + " " + urun4.fiyat
-			+ " " + urun4.stok);
+	System.out.println(urun4.marka + " " + urun4.model + " " + urun4.renk + " " 
+						+ urun4.hafiza + " " + urun4.fiyat	+ " " + urun4.stok);
 	Odev6Obje urun5 = new Odev6Obje("Huawei", "PLite", "Rosa", 128, 160, 56);
 
-	System.out.println(urun5.marka + " " + urun5.model + " " + urun5.renk + " " + urun5.hafiza + " " + urun5.fiyat
-				+ " " + urun5.stok);
+	System.out.println(urun5.marka + " " + urun5.model + " " + urun5.renk + " " 
+						+ urun5.hafiza + " " + urun5.fiyat + " " + urun5.stok);
 
 		Odev6Obje urun = new Odev6Obje();
 
@@ -68,6 +75,8 @@ public class Odev6Telefon {
 		System.out.println("Stok durumunu belirtiniz: ");
 		urun6.stok = scan.nextInt();
 
+		Odev6Obje urun7 = new Odev6Obje();
+		System.out.println(urun7.marka + urun7.model);
 	}
 
 }
