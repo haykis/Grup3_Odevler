3ac8753ccda155334f34d04e604c980606f0833e
ramazan <ramaz@DESKTOP-5G57CJS.fritz.box>
2022-12-26 21:24:43 +0100

    6.Hafta ödevim

---
+++ src/ramazanKombe/Odev6Obje.java
@@ -0,0 +1,33 @@
+package ramazanKombe;
+
+public class Odev6Obje {
+	
+	public String marka ;
+	public String model ;
+	public int hafiza;
+	public String renk;
+	public double fiyat;
+	public int stok;
+	
+	
+	
+public Odev6Obje(String string, String string2, String string3, int i, int j, int k) {};
+	
+
+	// Parametreli constructor
+
+
+	public Odev6Obje() {
+		super();
+		this.marka = marka;
+		this.model = model;
+		this.hafiza = hafiza;
+		this.renk = renk;
+		this.fiyat = fiyat;
+		this.stok = stok;
+	}
+}
+
+	
+
+
--- src/ramazanKombe/Odev6Telefon.java
+++ src/ramazanKombe/Odev6Telefon.java
@@ -1,9 +1,6 @@
 package ramazanKombe;
 
-import java.util.Iterator;
 import java.util.Scanner;
-
-import aliMustafaZengin.Odev6Product;
 
 public class Odev6Telefon {
 
@@ -11,31 +8,31 @@ public class Odev6Telefon {
 
 
 
-//		Odev6Product urun1 = new Odev6Product("Samsung", "Galaxy", "Rosa", 512, 600, 78);
-//
-//		System.out.println(urun1.marka + " " + urun1.model + " " + urun1.renk + " " + urun1.hafiza + " " + urun1.fiyat
-//				+ " " + urun1.stok);
-//
-//		Odev6Product urun2 = new Odev6Product("Samsung", "Galaxy", "Gold", 128, 500, 100);
-//
-//		System.out.println(urun2.marka + " " + urun2.model + " " + urun2.renk + " " + urun2.hafiza + " " + urun2.fiyat
-//				+ " " + urun2.stok);
-//
-//		Odev6Product urun3 = new Odev6Product("Lenovo", "Legion", "Siyah", 256, 400, 200);
-//
-//		System.out.println(urun3.marka + " " + urun3.model + " " + urun3.renk + " " + urun3.hafiza + " " + urun3.fiyat
-//				+ " " + urun3.stok);
-//
-//		Odev6Product urun4 = new Odev6Product("Iphone", "13 Promax", "Rosa", 512, 900, 80);
-//
-//		System.out.println(urun4.marka + " " + urun4.model + " " + urun4.renk + " " + urun4.hafiza + " " + urun4.fiyat
-//				+ " " + urun4.stok);
-//		Odev6Product urun5 = new Odev6Product("Huawei", "PLite", "Rosa", 128, 160, 56);
-//
-//		System.out.println(urun5.marka + " " + urun5.model + " " + urun5.renk + " " + urun5.hafiza + " " + urun5.fiyat
-//				+ " " + urun5.stok);
+	Odev6Obje urun1 = new Odev6Obje("Samsung", "Galaxy", "Rosa", 512, 600, 78);
 
-		Odev6Product urun = new Odev6Product();
+	System.out.println(urun1.marka + " " + urun1.model + " " + urun1.renk + " " + urun1.hafiza + " " + urun1.fiyat
+			+ " " + urun1.stok);
+
+	Odev6Obje urun2 = new Odev6Obje("Samsung", "Galaxy", "Gold", 128, 500, 100);
+
+	System.out.println(urun2.marka + " " + urun2.model + " " + urun2.renk + " " + urun2.hafiza + " " + urun2.fiyat
+			+ " " + urun2.stok);
+
+		Odev6Obje urun3 = new Odev6Obje("Lenovo", "Legion", "Siyah", 256, 400, 200);
+
+	System.out.println(urun3.marka + " " + urun3.model + " " + urun3.renk + " " + urun3.hafiza + " " + urun3.fiyat
+			+ " " + urun3.stok);
+
+	Odev6Obje urun4 = new Odev6Obje("Iphone", "13 Promax", "Rosa", 512, 900, 80);
+
+	System.out.println(urun4.marka + " " + urun4.model + " " + urun4.renk + " " + urun4.hafiza + " " + urun4.fiyat
+			+ " " + urun4.stok);
+	Odev6Obje urun5 = new Odev6Obje("Huawei", "PLite", "Rosa", 128, 160, 56);
+
+	System.out.println(urun5.marka + " " + urun5.model + " " + urun5.renk + " " + urun5.hafiza + " " + urun5.fiyat
+				+ " " + urun5.stok);
+
+		Odev6Obje urun = new Odev6Obje();
 
 		urun.marka = "Samsung";
 		urun.model = "Galaxy";
@@ -50,7 +47,7 @@ public class Odev6Telefon {
 
 		Scanner scan = new Scanner(System.in);
 
-		Odev6Product urun6 = new Odev6Product();
+		Odev6Obje urun6 = new Odev6Obje();
 
 		System.out.println("Urunun markasini giriniz: ");
 
