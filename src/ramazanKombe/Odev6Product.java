1953f7f5e9e42a22941488a9aa61530e1e4134f3
ramazan <ramaz@DESKTOP-5G57CJS.fritz.box>
2022-12-26 18:17:25 +0100

    6. Hafta Odevim

---
+++ src/ramazanKombe/Odev6Product.java
@@ -0,0 +1,26 @@
+package ramazanKombe;
+
+public class Odev6Product {
+	String marka;
+	String model;
+	int hafiza;
+	String renk;
+	double fiyat;
+	int stok;
+
+	// Parametreli constructor
+	public Odev6Product()
+	 {
+		this.marka = marka;
+		this.model = model;
+		this.hafiza = hafiza;
+		this.renk = renk;
+		this.fiyat = fiyat;
+		this.stok = stok;
+	}
+
+	}
+
+	
+
+
---
+++ src/ramazanKombe/Odev6Telefon.java
@@ -0,0 +1,74 @@
+package ramazanKombe;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Odev6Telefon {
+
+	public static void main(String[] args) {
+
+
+
+//		Odev6Product urun1 = new Odev6Product("Samsung", "Galaxy", "Rosa", 512, 600, 78);
+//
+//		System.out.println(urun1.marka + " " + urun1.model + " " + urun1.renk + " " + urun1.hafiza + " " + urun1.fiyat
+//				+ " " + urun1.stok);
+//
+//		Odev6Product urun2 = new Odev6Product("Samsung", "Galaxy", "Gold", 128, 500, 100);
+//
+//		System.out.println(urun2.marka + " " + urun2.model + " " + urun2.renk + " " + urun2.hafiza + " " + urun2.fiyat
+//				+ " " + urun2.stok);
+//
+//		Odev6Product urun3 = new Odev6Product("Lenovo", "Legion", "Siyah", 256, 400, 200);
+//
+//		System.out.println(urun3.marka + " " + urun3.model + " " + urun3.renk + " " + urun3.hafiza + " " + urun3.fiyat
+//				+ " " + urun3.stok);
+//
+//		Odev6Product urun4 = new Odev6Product("Iphone", "13 Promax", "Rosa", 512, 900, 80);
+//
+//		System.out.println(urun4.marka + " " + urun4.model + " " + urun4.renk + " " + urun4.hafiza + " " + urun4.fiyat
+//				+ " " + urun4.stok);
+//		Odev6Product urun5 = new Odev6Product("Huawei", "PLite", "Rosa", 128, 160, 56);
+//
+//		System.out.println(urun5.marka + " " + urun5.model + " " + urun5.renk + " " + urun5.hafiza + " " + urun5.fiyat
+//				+ " " + urun5.stok);
+
+		Odev6Product urun = new Odev6Product();
+
+		urun.marka = "Samsung";
+		urun.model = "Galaxy";
+		urun.renk = "Rosa";
+		urun.hafiza = 512;
+		urun.fiyat = 600;
+		urun.stok = 78;
+
+		System.out.println(urun.marka + " " + urun.model + " " + urun.renk + " " + urun.hafiza + " " + urun.fiyat+ urun.stok);
+
+		// Kullanicidan veri alma
+
+//		Scanner scan = new Scanner(System.in);
+//
+//		Product urun6 = new Product();
+//
+//		System.out.println("Urunun markasini giriniz: ");
+//
+//		String marka = (urun6).next();
+//		System.out.println("Urunun modelini giriniz: ");
+//
+//		String model = urun6.next();
+//
+//		System.out.println("Bir renk belirtiniz: ");
+//		String renk = urun6.next();
+//
+//		System.out.println("Hafiza belirtiniz: ");
+//		int hafiza = urun6.nextInt();
+//
+//		System.out.println("Fiyat giriniz: ");
+//		double fiyat = urun6.nextDouble();
+//
+//		System.out.println("Stok durumunu belirtiniz: ");
+//		int stok = urun6.nextInt();
+//
+	}
+
+}
