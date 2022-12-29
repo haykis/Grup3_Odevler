2eb377a1bf62f3566c3c91d2bafe0e080c40a68b
Jasi <Jasi@Asi1987>
2022-12-29 21:34:06 +0100

    Odev7

---
+++ src/aliMustafaZengin/Odev7_2_Counter.java
@@ -0,0 +1,25 @@
+package aliMustafaZengin;
+
+
+	class Odev7_2_Counter{
+		 int count=0;
+		
+		Odev7_2_Counter(){
+			count++;
+System.out.println(count);			
+		}
+	
+	
+	public static void main(String[] args) {
+		
+		Odev7_2_Counter c1= new Odev7_2_Counter();//1
+		
+		Odev7_2_Counter c2= new Odev7_2_Counter();//1
+		
+		Odev7_2_Counter c3= new Odev7_2_Counter();//1
+		
+		
+
+	}
+
+}
