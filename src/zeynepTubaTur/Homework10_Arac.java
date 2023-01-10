040242e8c853bc5effbb7c224a7fd326f9d303d8
tubatur <ztubatur@gmail.com>
2023-01-10 15:37:36 +0100

    Homework10

---
+++ src/zeynepTubaTur/Homework10_Arac.java
@@ -0,0 +1,53 @@
+package zeynepTubaTur;
+
+public class Homework10_Arac {
+
+	private String ad_soyad;
+	private String arac_marka;
+	private String plaka;
+	private String adres;
+	private String arac_durumu;
+
+	// Getter
+	public String getAd_soyad() {
+		return ad_soyad;
+	}
+
+	// Setter
+	public void setAd_soyad(String ad_soyad) {
+		this.ad_soyad = ad_soyad;
+	}
+
+	public String getArac_marka() {
+		return arac_marka;
+	}
+
+	public void setArac_marka(String arac_marka) {
+		this.arac_marka = arac_marka;
+	}
+
+	public String getPlaka() {
+		return plaka;
+	}
+
+	public void setPlaka(String plaka) {
+		this.plaka = plaka;
+	}
+
+	public String getAdres() {
+		return adres;
+	}
+
+	public void setAdres(String adres) {
+		this.adres = adres;
+	}
+
+	public String getArac_durumu() {
+		return arac_durumu;
+	}
+
+	public void setArac_durumu(String arac_durumu) {
+		this.arac_durumu = arac_durumu;
+	}
+
+}
