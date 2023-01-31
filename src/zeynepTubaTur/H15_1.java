08507cdb072c1bf79bfb1bd6760487d99e62858d
tuba <tuba@tubas-mac-mini.fritz.box>
2023-01-31 15:12:45 +0100

    Homework15

---
+++ src/zeynepTubaTur/H15_1.java
@@ -0,0 +1,43 @@
+package zeynepTubaTur;
+
+public class H15_1 {
+	
+//	1)AbstractClass-Interface TekrarSorulari
+	
+//	1_Abstract class kullanilarak obje olusturamayiz.  True
+	
+//	2_Interface kullanilarak obje olusturabiliriz.  False
+	
+//	3_public abstract int sumOfTwo(int n1, int n2) { } True/False
+	
+//	4_public class Animal { public abstract void sound() } True
+	
+//	5_public abstract class Animal {
+	
+//	public abstract void eat(); 
+//	public void sound(){ } } True
+	
+//	6_ Abstract class final olarak tanimlanamaz.  True
+	
+//	7_ public abstract class Animal {  
+	
+//	private abstract void sound(); } True
+	
+//	8_ Java birden fazla class’a extends ile inherit etmenize izin 
+//	vermez.
+	
+//	True. Bir class bir super classi extends edebilir. Bir child classin, bir parent classi olur.
+	
+//	9_ Java birden fazla interface’e implements ile inherit etmenize izin 
+//	verir. 
+	
+//	True. Bir class dan istediginiz kadar interface i inherit edebilirsiniz. Coklu inheritance ' a uygundur.
+	
+//	10_ Bir interface abstract veya concrete method’lara sahip olabilir. 
+
+//	True. Bir interface sadece abstract method’lara sahipken java guncellemesinden sonra,
+//	artik concrete method’lara da sahip olabiliyor.
+//	Non-static bir concrete method olusturmak icin, default kelimesini kullanmamiz gerekiyor.
+//	Buradaki default bizim onceden ogrendigimiz default demek degildir.
+
+}
